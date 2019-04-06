0a2604bef1b9bb4dcfb766f9f3a6d78888953422
2 <2>
2019-04-06 22:36:23 +0200

    Added base command which only implements the CnaexecuteChanged

---
+++ WpfHelperLibrary/Commands/BaseCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfHelperLibrary.Commands
+{
+	public abstract class BaseCommand : ICommand
+	{
+
+		/// <summary>
+		/// Wire up command to the Commandmanager
+		/// </summary>
+		public event EventHandler CanExecuteChanged
+		{
+			//This is done to wire up to the WPF command center
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		public abstract bool CanExecute(object parameter);
+
+		public abstract void Execute(object parameter);
+	}
+}
